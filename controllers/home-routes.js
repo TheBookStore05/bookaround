373e58326bd8799d402a24521cb2a34f9f2ebf19
satwider47 <satwinder47@outlook.com>
2022-06-02 17:12:00 -0400

    files attached to handlebars

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,21 @@
 const router = require('express').Router();
 
 router.get('/',(req,res) => {
-    console.log("hello");
-    res.render('homepage');
+    console.log('=================');
+    return res.render('homepage');
 });
+router.get('/checkout1',(req,res) => {
+    console.log('=================');
+    return res.render('checkout1');
+});
+router.get('/favourites',(req,res) => {
+    console.log('=================');
+    return res.render('favourites');
+});
+router.get('/signin',(req,res) => {
+    console.log('=================');
+    return res.render('signin');
+});
+
 
 module.exports = router;
